85b95fd8b4c516bedb559af2190ca0303520ef1b
Clive Chan <cc@clive.io>
2017-09-18 15:02:14 -0400

    Refactor to have FileParser class

--- boole/parser.py
+++ boole/parser.py
@@ -2,109 +2,99 @@
 boole.py
 """
 
-import boolexpr
+import expr
 import re
 
-# boolexpr.parse("a | b & c => d | b <=> asdf")
-
-# Each state:
-# state number [int]:
-#   array of
-#     (condition [regex], doing [callback that gets match()], transition_to [int])
-
-EOF = "###EOF###"
+class BooleSyntaxError(SyntaxError):
+    pass
 
 
 def state(expecting=None):
-    def func_modifier(currstate):
-        def modified_func(filename, linenum, orig_line):
+    def func_modifier(state):
+        def modified_func(self, filename, linenum, orig_line):
             line = orig_line.strip()
             if line.startswith("%") or len(line) == 0:
-                return globals()[currstate.__name__]
+                return getattr(self, state.__name__)
             try:
-                transition_to = currstate(line)
-            except SyntaxError as e:
-                raise SyntaxError("Error raised in state " + currstate.__name__ + ": " + repr(e),
-                                  (filename, linenum, 0, orig_line))
+                transition_to = state(self, line)
+            except BooleSyntaxError as e:
+                raise BooleSyntaxError("Error raised in state " + state.__name__ + ": " + repr(e),
+                                (filename, linenum, 0, orig_line))
             if transition_to is None:
                 if expecting is not None:
-                    raise SyntaxError("Expected " + expecting, (filename, linenum, 0, orig_line))
+                    raise BooleSyntaxError("Expected " + expecting, (filename, linenum, 0, orig_line))
                 else:
-                    raise SyntaxError("Unexpected line", (filename, linenum, 0, orig_line))
+                    raise BooleSyntaxError("Unexpected line", (filename, linenum, 0, orig_line))
             else:
                 return transition_to
 
-        modified_func.__name__ = currstate.__name__
+        modified_func.__name__ = state.__name__
         return modified_func
 
     return func_modifier
 
 
-@state(expecting="#u declaration")
-def initial(line):
-    if line.startswith("#u "):
-        D["users"] = line[3:].split(' ')
-        return user_declared
+class FileParser:
+    def __init__(self, file):
+        self.state = self.initial
+        self.D = {}
+        self.EOF = "###EOF###"
+        self.users = None
+        self.assignment = None
 
+        for i, orig_line in enumerate(file):
+            print("Current state:", self.state.__name__, "\nNew line of code:", orig_line)
+            self.state = self.state(filename, i + 1, orig_line)
+        self.state = self.state(filename, i + 2, self.EOF)
+        if self.state is True:
+            print("Successfully exited.")
 
-@state(expecting="#a declaration")
-def user_declared(line):
-    if line.startswith("#a "):
-        D["assignment"] = line[3:]
-        return header_declared
+    @state(expecting="#u declaration")
+    def initial(self, line):
+        if line.startswith("#u "):
+            self.users = line[3:].split(' ')
+            return self.user_declared
 
+    @state(expecting="#a declaration")
+    def user_declared(self, line):
+        if line.startswith("#a "):
+            self.assignment = line[3:]
+            return self.header_declared
 
-@state(expecting="#q declaration")
-def header_declared(line):
-    if line.startswith("#q "):
-        D["currentQuestion"] = line[3:]
-        if not D["currentQuestion"].startswith(D["assignment"]):
-            raise SyntaxError("Question number does not start with assignment number.")
-        else:
-            return question_declared
-    print("reached end")
+    @state(expecting="#q declaration")
+    def header_declared(self, line):
+        if line.startswith("#q "):
+            self.currentQuestion = line[3:]
+            if not self.currentQuestion.startswith(self.assignment):
+                raise BooleSyntaxError("Question number does not start with assignment number.")
+            else:
+                return self.question_declared
+        print("reached end")
 
+    @state(expecting="#check declaration")
+    def question_declared(self, line):
+        if line == self.EOF:
+            return True
+        if line.startswith("#check "):
+            check = line[7:]
+            if check == "NONE":
+                return self.check_NONE
+            else:
+                raise BooleSyntaxError("The #check \"" + check + "\" is invalid")
 
-@state(expecting="#check declaration")
-def question_declared(line):
-    if line == EOF:
-        return True
-    if line.startswith("#check "):
-        check = line[7:]
-        if check == "NONE":
-            return check_NONE
-        else:
-            raise SyntaxError("The #check \"" + check + "\" is invalid")
-
-
-@state(expecting="solution lines")
-def check_NONE(line):
-    if line.startswith("#q "):
-        pass
-    elif line.startswith("#a "):
-        pass
-    elif line == EOF:
-        return True
-    else:  # it's a line of solution!
-        print("Line of solution in check_NONE!", line)
-        return check_NONE
-
-
-state = initial
-D = {}
-
-
-def parse(file):
-    global state
-    for i, orig_line in enumerate(file):
-        print("Current state:", state.__name__, "\nNew line of code:", orig_line)
-        state = state(filename, i + 1, orig_line)
-    state = state(filename, i + 2, EOF)
-    if state is True:
-        print("Successfully exited.")
-
+    @state(expecting="solution lines")
+    def check_NONE(self, line):
+        if line.startswith("#q "):
+            pass
+        elif line.startswith("#a "):
+            pass
+        elif line == self.EOF:
+            return True
+        else:  # it's a line of solution!
+            print("Line of solution in check_NONE!", line)
+            return self.check_NONE
 
 if __name__ == "__main__":
-    filename = "testgood.boole"  # input("Filename: ")
+    filename = "tests/testgood.boole"  # input("Filename: ")
     with open(filename) as file:
-        parse(file)
+        parsed = FileParser(file)
